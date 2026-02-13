8f425cd543de9013a0436ddbadde5907174b47b3
Likhith <likhithraj005@gmail.com>
2026-02-13 18:21:56 +0530

    commiting the config server

---
+++ configm-server/src/test/java/com/likhith/microservices/configserver/ConfigmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likhith.microservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
