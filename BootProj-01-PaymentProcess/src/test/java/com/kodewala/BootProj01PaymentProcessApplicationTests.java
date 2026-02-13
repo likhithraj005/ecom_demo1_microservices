34551a066dc37fcabc94a8bb8dde5f7b185452b6
Pijus <pijusjena@gmail.com>
2026-02-13 18:05:42 +0530

    added new seevice

---
+++ BootProj-01-PaymentProcess/src/test/java/com/kodewala/BootProj01PaymentProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodewala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj01PaymentProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
